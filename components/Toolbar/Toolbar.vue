b12f6ec1a65ea4a56a1e21de1d0117832f32d844
xFakir <490679728@qq.com>
2020-08-17 22:24:59 +0800

    feat[Toolbar] 完善工具栏

--- components/Toolbar/Toolbar.vue
+++ components/Toolbar/Toolbar.vue
@@ -1,16 +1,67 @@
 <template>
   <div class="Toolbar">
-    <xcard>
+    <xcard class="tool-card">
       <div class="title">
         title
       </div>
       <div class="divide"></div>
       <div class="content">
-        <ul>
-          <li>aa</li>
-          <li>bb</li>
-          <li>cc</li>
-        </ul>
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
+      </div>
+    </xcard>
+    <xcard class="tool-card">
+      <div class="title">
+        title
+      </div>
+      <div class="divide"></div>
+      <div class="content">
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
+      </div>
+    </xcard>
+    <xcard class="tool-card">
+      <div class="title">
+        title
+      </div>
+      <div class="divide"></div>
+      <div class="content">
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
+      </div>
+    </xcard>
+    <xcard class="tool-card">
+      <div class="title">
+        title
+      </div>
+      <div class="divide"></div>
+      <div class="content">
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
+        <a href="#">aaa</a>
+        <a href="#">bbb</a>
+        <a href="#">ccc</a>
+        <a href="#">ddd</a>
       </div>
     </xcard>
   </div>
@@ -25,6 +76,9 @@ export default {
 </script>
 
 <style scoped>
+.tool-card {
+  padding: 10px;
+}
 .divide {
   background-color: aqua;
   height: 1px;
